ab0b02e9df0a505d0fb3f448f9e07f3fb29af85f
root <1339298669@qq.com>
2019-06-02 21:06:58 +0800

    L

--- logisticsSSM/src/main/java/com/zking/ssm/service/EmpService.java
+++ logisticsSSM/src/main/java/com/zking/ssm/service/EmpService.java
@@ -22,15 +22,47 @@ public interface EmpService {
 
     int updateByPrimaryKey(Emp var1);
 
+    /**
+     * 登录
+     * @param var1
+     * @return
+     */
     Emp loginEmp(Emp var1);
 
+    /**
+     * 获得所有菜单
+     * @param var1
+     * @return
+     */
     List<Menu> getMens(String var1);
+
+    /**
+     * 获得所有员工，根据地域
+     * @param var1
+     * @return
+     */
 
     List<Emp> getEmps(String var1);
 
+    /**
+     * 增加员工
+     * @param var1
+     * @return
+     */
     int addEmp(Emp var1);
 
+    /**
+     * 获得最大id
+     * @return
+     */
     int getMaxeid();
 
-    int addER(Integer var1);
+    /**
+     * 增加角色
+     * @param eid，@param rid
+     * @return
+     */
+    int addER(Integer eid,Integer rid);
+
+    public List<Menu> getMenus2(Emp emp);
 }
